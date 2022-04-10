ed9ba327147aed510db2bf54bcb9c77f3d56048e
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-10 18:16:29 +0100

    Output using put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+
+
 /**
  * main - Entry point 
- *
+ * Description : output using puts function
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
+	
 	puts("\"Programming is like building a miltilingual puzzle");
 	return (0);
+
 }	
