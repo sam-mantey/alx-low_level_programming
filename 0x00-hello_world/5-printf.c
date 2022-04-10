b795260eef17172948c45511d8d4893edd2bf2dc
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-10 19:23:50 +0100

    Using the printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,10 @@
 /**
  * main - Entry point
  * Description : Using the printf function
- * Return : Always 0 (Succes)
+ * Return : Always 0 (Success/prints the sentence)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
-			
+}			
