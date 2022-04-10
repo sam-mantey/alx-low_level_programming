eec5b287937bceec7013c5a74071edc512818777
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-10 19:32:05 +0100

    Using the printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,10 @@
 
 /**
  * main - Entry point
- * Description : Using the printf function
+ *
+ * Description : Using the printf function 
+ * Printing a sentence to the screen
+ *
  * Return : Always 0 (Success/prints the sentence)
  */
 int main(void)
