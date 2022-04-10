6c3f208747bfe5427238353da84d18d7651cf2b2
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-10 20:05:38 +0100

    Using the printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - It prints the statement (with,...)to the sreen
+ * main - It prints the statement (with,.with proper..) to the sreen
  *
  * Description : Using the printf function
  * Printing a sentence to the screen
@@ -10,6 +10,6 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
