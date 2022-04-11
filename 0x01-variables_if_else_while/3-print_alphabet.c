5704df5a72bbbf2470f7c9fec2fc89687b19f013
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-11 16:35:35 +0100

    Alphabet game 2

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Succes/correct)
+ */
+int main(void)
+{
+	char low_alph;
+	char upper_alph;
+
+	for (low_alph = 'a'; low_alph <= 'z'; low_alph++)
+	{
+		putchar(low_alph);
+	}
+	for (upper_alph = 'A'; upper_alph <= 'Z'; upper_alph++)
+	{
+		putchar(upper_alph);
+	}
+	putchar('\n');
+	return (0);
+}
