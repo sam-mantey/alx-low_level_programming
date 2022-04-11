90b9034394430bdfe81e59406cfb7083510c65e6
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-11 17:07:27 +0100

    Removing lines from a loop

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes/correct)
+ *
+ */
+int main (void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph == 'q' || alph == 'e')
+		{
+			continue;
+		}
+	putchar(alph);
+	}
+	return (0);
+}
