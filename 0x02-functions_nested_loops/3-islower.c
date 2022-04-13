9f0d42e9a7e3acb68e46a28bbae3faffbb236349
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-13 22:58:37 +0100

    A fucnction to check if an input is in lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if an input is in lowercase
+ *
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	int c;
+
+	if (c > 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else 
+	{
+	return (0);
+	}
+}
