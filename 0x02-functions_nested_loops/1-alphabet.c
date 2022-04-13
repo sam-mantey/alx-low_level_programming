668227ddd7b4a5bcf3fefe6237b25447accbc3e6
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-13 22:19:11 +0100

    print_alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Check the code
+ *
+ * Return: Always zero
+ */
+void print_alphabet(void)
+{
+	char alph
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
