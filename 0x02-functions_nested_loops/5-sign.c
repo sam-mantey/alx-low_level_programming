9cb373281471030dd12d5f9fa46184a9d1de9e12
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-13 23:23:34 +0100

    A function to print the sign of an integer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sigh of an integer
+ * @n: Contains integer to be compared
+ * Return: always 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	}
+	else if (n == 0 )
+	{
+	_putchar('0');
+	}
+	else if
+	{
+	_putchar('-');
+	}
+	_putchar('\n');
+}
