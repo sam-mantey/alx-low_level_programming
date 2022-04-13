81a727f747017832e804d2f110f815f1301ea221
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-13 23:43:42 +0100

    Returning the absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - this returns the absolute value of an input
+ *
+ * Return: Always 0
+ */
+int _abs(int)
+{
+	if (n < 0)
+	{
+	return (n * (-1));
+	}
+	else if (n == o)
+	{
+	return (0);
+	}
+	else
+	{
+	return (n);
+	}
+}
