e13d9c15f236aa9b3737d961e6ec444d01b83b7a
sam-mantey <98030457+sam-mantey@users.noreply.github.com>
2022-04-16 05:31:56 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,23 @@
 #include "main.h"
-
 /**
- * print_most_numbers - Prints without 2 and 4
+ * print_most_numbers - print numbers
  *
- * Return: Always 0 (Success/correct)
+ * Return: Always 0.
  */
 void print_most_numbers(void)
 {
-	int i;
 
-	i = 0;
-	while (i < 10)
-	{
-	if (i == 2 || i == 4 )
-	{
-	continue;
-	}
-	i++;
-	_putchar(i);
-	}
-	_putchar('\n');
+int i;
+
+i = 0;
+while (i < 10)
+{
+if (i != 2 && i != 4)
+{
+_putchar (i + '0');
+}
+i++;
+}
+
+_putchar ('\n');
 }
