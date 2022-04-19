8193872a94477e982f65b9977395afdfc28e5fa6
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-19 11:24:08 +0100

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (j = 0; j <= 14; j++)
+	{
+	_putchar(j);
+	}
+	_putchar('\n');
+	}
+}
