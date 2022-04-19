d0a4364dd1b221b5c898b5a38b1ecdedc5e7f127
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-19 12:19:16 +0100

    Commenting mistake

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* i does not increase and so i is always less than 10
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
+}
