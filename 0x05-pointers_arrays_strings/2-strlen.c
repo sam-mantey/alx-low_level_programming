02809a91314d35665cca16947d48ec153212e473
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-20 23:33:37 +0100

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the legth of a string
+ * @s: It contains the string
+ * Result: Always 0 (success)
+ */
+int _strlen(char *s)
+{
+	_putchar(%lu, sizeof(*s));
+}
