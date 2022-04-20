848f7ec9288e26c9e4b687fb95ad4eb0935a9694
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-20 23:15:02 +0100

    diffirencing

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,10 @@
 
 /**
  * reset_to_98 - Differencing the a pointer
- *
+ * @n: A pointer to an int variable
  * Return: Always 0 (success)
  */
 void reset_to_98(int *n)
 {
-	int *p;
-
-	p = &n;
-	*p = 98;
+	*n = 98;
 }
