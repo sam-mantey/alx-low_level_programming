bf3dbdaee0d7fb1bbbe69e730718457b85e734fe
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-21 02:19:36 +0100

    printing the content of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_array - It prints the array to the screen
+ * @n: It is the size of the array
+ * @a: it is the array name
+ * Return: Always 0 (success)
+ */
+void print_array(int *a, int n);
+{
+	int i;
+
+	for (i = 0; n - 1; i++)
+	{
+	printf("%d, \n", a[i]);
+	}
+	printf("\n");
+}
