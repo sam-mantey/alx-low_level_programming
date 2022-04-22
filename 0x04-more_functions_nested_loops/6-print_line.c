93f1770958a6699a21154321f53be6ed0e9bb775
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-22 18:57:25 +0100

    shortest distance between two lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,12 +11,14 @@ void print_line(int n);
 
 	if (n > 0)
 	{
-	for (i = 0; i <= n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 	_putchar(95);
 	}
 	_putchar('\n');
 	}
 	else
+	{
 	_putchar('\n');
+	}
 }
