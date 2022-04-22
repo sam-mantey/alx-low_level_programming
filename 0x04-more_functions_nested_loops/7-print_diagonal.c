0aedce629c712a76596edbf9733472b58909d905
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-22 20:20:05 +0100

    Printing diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - this draws a diagonal
+ * @n: is an int variable
+ * Return: Always 0 (success)
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+	for (i = 0; i < n; i++)
+	{
+	for (j = 0; j < i -1; j++)
+	{
+	_putchar(' ');
+	}
+	_putchar(92);
+	_putchar('\n');
+	}
+	}
+	else 
+	_putchar('\n');
+}
