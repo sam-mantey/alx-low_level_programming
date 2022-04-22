f5e13289411d5d06d7d85bd2cd794c4f4d8da920
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-22 18:49:03 +0100

    shortest distance between two lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - this function draws a straight line
+ * @n: an int
+ * Return: Always 0 (success)
+ */
+void print_line(int n);
+{
+	int i;
+
+	if (n > 0)
+	{
+	for (i = 0; i <= n - 1; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+	}
+	else
+	_putchar('\n');
+}
