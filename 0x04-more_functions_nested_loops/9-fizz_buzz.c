ea4925f87bebb0f2e8bd19a52db2852ea3e44c03
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-23 13:16:01 +0100

    fizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+	if (i % 3 == 0 && i % 5 != 0)
+	{
+	_putchar('Fizz');
+	}
+	else if (i % 5 == 0 && i % 3 != 0)
+	{
+	_putchar('Buzz');
+	}
+	else if (i % 5 == 0 && i % 3 == 0)
+	{
+	_putchar('FizzBuzz');
+	}
+	_putchar(' \n');
+	}
+}
