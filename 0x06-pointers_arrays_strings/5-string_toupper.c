d3c4a3481e4dcf6ed0bf432cf4a8c6b68f282fcc
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-24 23:51:42 +0100

    changing from lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes from lowercase to uppercase
+ * @str: it is a string
+ * Return: returns a char
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+	str[i] = srt[i] -32;
+	}
+	}
+	return (str);
+}
