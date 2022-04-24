c71acba8a45bd40920430e64c931ca1f5098c3dc
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-24 17:10:03 +0100

    0.strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
+	int i, j;
 
 	i = 0;
-	j = 0;
 	while (dest[i] != '\0')
 	{
 	i++;
 	}
+	j = 0;
 	while (src[j] != '\0')
 	{
 	dest[i] = src[j];
@@ -23,5 +23,6 @@ char *_strcat(char *dest, char *src)
 	j++;
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
