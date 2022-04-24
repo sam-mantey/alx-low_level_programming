58830d5577b7a1bb8182f6ebb855ac5b7a95a485
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-04-24 16:59:14 +0100

    0.strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 /**
  * _strcat - concatenates two strings
- * @dest: contains string
- * @ src: contains string
+ * @dest: a pointer that contains string
+ * @ src: a pointer that contains string
  * Return: Always 0 (success)
  */
 char *_strcat(char *dest, char *src)
@@ -11,11 +11,16 @@ char *_strcat(char *dest, char *src)
 	int i;
 
 	i = 0;
-	while (i != '\0')
+	while (dest[i] != '\0')
 	{
-	src[i] = dest[i];
 	i++;
 	}
+	while (src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
 	dest[i] = '\0';
-	return dest;
+	return (dest);
 }
