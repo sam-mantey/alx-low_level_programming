fd05c4d0577e9c39c6d2b753d9460095f49d7c64
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-05-03 12:43:42 +0100

    solving indicies

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - This finds the result of a number raised to a power
+ * @x: the base
+ * @y: the exponent
+ * Return: the results
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+	{
+	return (x);
+	}
+	else if (y == 0)
+	{
+	return (1);
+	}
+	else if (y < 0)
+	{
+	return (x * _pow_recursion(x, y - 1));
+}
