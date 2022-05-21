f20541d9acff74dcef8826292fb840dd9d0615aa
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-05-21 08:31:49 +0100

    Printing the programme name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,12 @@
  * main - Prints the name of the programme
  * @argc: Count arguments to main
  * @argv: a char array of the agruments to main
+ * Return: Always 0
  */
 
 int main(int argc, char *argv[])
 {
-
+	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
