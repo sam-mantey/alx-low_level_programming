e5d11438cd15a78ca36bc7ffc8a9386907a59c62
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-05-21 10:46:54 +0100

    multiplying two int input

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers
+ * @argc: It counts the argument passed to mmain
+ * @argv: It is an array of arguments passed to main
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int prod;
+
+	if (argc != 3)
+	{
+	printf("Error\n");
+	}
+	prod = atoi(argv[1]) * atio(argv[2])
+	printf("%d\n", prod);
+	return (0);
+}
