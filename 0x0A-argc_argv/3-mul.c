c19c83b7f81933ee209358040b5a43927d186bc4
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-05-21 11:05:28 +0100

    multiplying two int input

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,17 +11,15 @@
 int main(int argc, char *argv[])
 {
 	int a, b;
-
-	if (argc != 3)
-	{
-	printf("Error\n");
-	return (1);
-	}
-	else if (argc == 2)
+	
+	if (argc == 2)
 	{
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
 	printf("%d\n", a * b);
 	return (0);
 	}
+	printf("Error\n");
+	return (1);
+
 }
