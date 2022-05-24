e805957f96c878bfbb835950241b6a0bf21d3b8c
sam-mantey <samuelmanteyasareadjei@gmail.com>
2022-05-24 06:25:38 +0100

    adding numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - adds integers
+ * @argv: this is an array of arguments passed to main
+ * @argc: this is the number of argumentds passed to main
+ * Return: Always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum, d;
+
+	if (argc == 1)
+	{
+	printf("0\n");
+	}
+
+	d = 0;
+	for (i = 1; i < argc; i++)
+	{
+	if (isdigit(argv[i] == 0))
+	{
+	d = d + 1;
+	}
+	}
+
+	if (d >= 1)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	else
+	{
+	j = 1;
+	sum = 0;
+	while (j < argc)
+	{
+	sum = sum + atoi(argv[j]);
+	j++;
+	}
+	return (0);
+	}
+}
